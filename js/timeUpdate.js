b42faac60e151e3d18f045abb02ff686c38eced9
Ronnyone1991 <rod21007@byui.edu>
2021-09-26 08:44:39 -0400

    new

--- js/timeUpdate.js
+++ js/timeUpdate.js
@@ -2,4 +2,4 @@ let hour = new Date(document.lastModified);
 document.getElementById("time").innerHTML = "Las Update: " + hour;
 // this is the current year 
 new Date().getFullYear()
-document.getElementById("currentYears").innerHTML = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
