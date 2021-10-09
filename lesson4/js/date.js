8183fdebc0336be4958692ad73ecb9da7ce00326
Ronnyone1991 <rod21007@byui.edu>
2021-10-09 16:17:03 -0400

    date

--- lesson4/js/date.js
+++ lesson4/js/date.js
@@ -1,1 +1,17 @@
-let 
+let now = new Date();
+// return the day of the week
+var d = new Date();
+  var weekday = new Array(7);
+  weekday[0] = "Sunday";
+  weekday[1] = "Monday";
+  weekday[2] = "Tuesday";
+  weekday[3] = "Wednesday";
+  weekday[4] = "Thursday";
+  weekday[5] = "Friday";
+  weekday[6] = "Saturday";
+
+  var n = weekday[d.getDay()];
+
+  document.getElementById("date").innerHTML = n;
+}
+
