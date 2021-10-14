82849dd1247e31386ebd49d03f5427b08ef5a009
Ronnyone1991 <rod21007@byui.edu>
2021-10-14 12:27:42 -0400

    new

---
+++ lesson5/js/bom.js
@@ -0,0 +1,29 @@
+const list = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+button.onclick = function () {
+  let myItem = input.value;
+  if (myItem.length > 0) {
+    input.value = "";
+
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const listBtn = document.createElement("buttom");
+
+    listItem.appendChild(listText);
+    listText.textContent = myItem;
+    listItem.appendChild(listBtn);
+    listBtn.textContent = "Delete";
+    list.appendChild(listItem);
+
+    listBtn.onclick = function (e) {
+      list.removeChild(listItem);
+    }
+
+    input.focus();
+  }
+  else {
+    alert("Please enter your favorite Book of mormom charpter")
+  }
+}
