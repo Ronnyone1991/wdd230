87ac991ec5ff9c248b47d88112a68e24861dbc55
Ronnyone1991 <rod21007@byui.com>
2021-10-20 00:42:31 -0430

    alt

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -2,4 +2,4 @@ let t = 54;
 let s = 12;
 let f = 35.47 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * s ** 0.16;
 let result = f.toFixed(0);
-document.getElementById("wind").innerHTML = result + " " + "F";
+document.getElementById("wind").innerHTML = result + " " + "°F";
