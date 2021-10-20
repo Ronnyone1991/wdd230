d377593b8a7577f8c55c934dee8552ce77e04cfc
Ronnyone1991 <rod21007@byui.com>
2021-10-20 00:40:28 -0430

    calculation

---
+++ lesson6/js/windchill.js
@@ -0,0 +1,5 @@
+let t = 54;
+let s = 12;
+let f = 35.47 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * s ** 0.16;
+let result = f.toFixed(0);
+document.getElementById("wind").innerHTML = result + " " + "F";
