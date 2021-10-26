17599bf67e4fa8b00f6b957897e6e4ca9dbbbcb6
Ronnyone1991 <rod21007@byui.edu>
2021-10-25 21:18:13 -0400

    new

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,6 +1,6 @@
 // windshill calculation
 let t = 54;
 let s = 12;
-let f = 35.47 + 0.6215 * t - 35.75 * s ** 0.16 + 0.4275 * s ** 0.16;
+let f = 35.47 + 0.6215 * t - 35.75 * (s ** 0.16) + 0.4275 * (s ** 0.16);
 let result = f.toFixed(0);
 document.getElementById("wind").innerHTML = result + " " + "°F";
