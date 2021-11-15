3e3631f2ef47ffaa9aca7f5f20f47b7f22c49878
Ronnyone1991 <rod21007@byui.edu>
2021-11-14 22:10:09 -0400

    vccffs

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -9,12 +9,14 @@ fetch(requestURL)
     console.table(jsonObject); // temporary checking for valid response and data parsing
     const prophet = jsonObject["prophets"];
     prophet.forEach((prophet) => {
+      // create elements
       let card = document.createElement("section");
       let h2 = document.createElement("h2");
       let img = document.createElement("img");
       let birthday = document.createElement("p");
       let birthplace = document.createElement("p");
 
+      // add JSON data and display it to the user
       h2.textContent = `${prophet.name} ${prophet.lastname}`;
       birthday.textContent = `Date of Birth: ${prophet.birthdate}`;
       birthplace.textContent = `Place of Birth: ${prophet.birthplace}`;
--- lesson-9/js/towns.js
+++ lesson-9/js/towns.js
@@ -1,33 +1,48 @@
-const requestURL =
-  "https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json";
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject); // temporary checking for valid response and data parsing
-    const prophet = jsonObject["prophets"];
-    prophet.forEach((prophet) => {
-      let card = document.createElement("section");
-      let h2 = document.createElement("h2");
-      let img = document.createElement("img");
-      let dob = document.createElement("p");
-      let pob = document.createElement("p");
+    console.table(jsonObject);
+    const town = jsonObject["towns"];
+    const listTown = ["Soda Spring", "Fish Heaven", "Preston"];
+    for (var i = 0; i < town.lenght; ++i);
+    {
+      if (listTown.includes(town[i].name)) {
+        let imgTown;
+        switch (town[i].name) {
+          case "Soda Spring":
+            imgTown = "./images/sodaspring.jpg";
+            break;
+          case "Fish Heaven":
+            imgTown = "./images/fishheaven.jpg";
+            break;
+          case "Preston":
+            imgTown = "./images/preston.jpg";
+            break;
+        }
 
-      h2.textContent = `${prophet.name} ${prophet.lastname}`;
-      dob.textContent = `Date of Birth: ${prophet.birthdate}`;
-      pob.textContent = `Place of Birth: ${prophet.birthplace}`;
-      img.setAttribute("src", prophet.imageurl);
-      img.setAttribute(
-        "alt",
-        `${prophet.name} ${prophet.lastname} - ${prophet.order}`
-      );
+        let townjson = document.createElement("section");
+        let h1 = document.createElement("h1");
+        let h2 = document.createElement("h2");
+        let img = document.createElement("img");
+        let yearf = document.createElement("p");
+        let population = document.createElement("p");
 
-      card.appendChild(h2);
-      card.appendChild(dob);
-      card.appendChild(pob);
-      card.appendChild(img);
-      document.querySelector(".cards").appendChild(card);
-    });
+        h1.textContent = `${town[i].name}`;
+        h2.textContent = `${town[i].motto} `;
+        yearf.textContent = `Year Founded: ${town[i].yearFounded}`;
+        population.textContent = `Current Population: ${town[i].currentPopulation}`;
+        img.setAttribute("src", imgTown);
+        img.setAttribute("alt", `${town[i].name} `);
+        townjson.appendChild(h1);
+        townjson.appendChild(h2);
+        townjson.appendChild(yearf);
+        townjson.appendChild(population);
+        townjson.appendChild(img);
+        document.querySelector(".towns").appendChild(townjson);
+      }
+    }
   });
