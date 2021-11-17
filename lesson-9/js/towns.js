1268580c69ea61f1ec4923bc36887abe7e0f001c
Ronnyone1991 <rod21007@byui.edu>
2021-11-16 23:52:34 -0400

    new

--- lesson-9/js/towns.js
+++ lesson-9/js/towns.js
@@ -1,22 +1,21 @@
-const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
+const townsRequestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 
-fetch(requestURL)
+fetch(townsRequestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);
     const town = jsonObject["towns"];
-    const listTown = ["Soda Spring", "Fish Heaven", "Preston"];
-    for (var count = 0; count < town.lenght; count++);
-    {
+    const listTown = ["Soda Springs", "Fish Haven", "Preston"];
+
+    for (let count = 0; count < town.length; count++) {
       if (listTown.includes(town[count].name)) {
         let imgTown;
         switch (town[count].name) {
-          case "Soda Spring":
+          case "Soda Springs":
             imgTown = "./images/sodaspring.jpg";
             break;
-          case "Fish Heaven":
+          case "Fish Haven":
             imgTown = "./images/fishheaven.jpg";
             break;
           case "Preston":
@@ -30,17 +29,20 @@ fetch(requestURL)
         let img = document.createElement("img");
         let yearf = document.createElement("p");
         let population = document.createElement("p");
+        let annualRain = document.createElement("p");
 
         h1.textContent = `${town[count].name}`;
         h2.textContent = `${town[count].motto} `;
         yearf.textContent = `Year Founded: ${town[count].yearFounded}`;
         population.textContent = `Current Population: ${town[count].currentPopulation}`;
+        annualRain.textContent = `Annual Rain Fall: ${town[count].averageRainfall}`;
         img.setAttribute("src", imgTown);
         img.setAttribute("alt", `${town[count].name} `);
         townjson.appendChild(h1);
         townjson.appendChild(h2);
         townjson.appendChild(yearf);
         townjson.appendChild(population);
+        townjson.appendChild(annualRain);
         townjson.appendChild(img);
         document.querySelector(".towns").appendChild(townjson);
       }
