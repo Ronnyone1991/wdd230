6c5cfdbec5c876f8a7ae1c10d631e35040073dff
Ronnyone1991 <rod21007@byui.edu>
2021-11-20 22:05:18 -0400

    new api

--- lesson-10/weatherapi.js
+++ lesson-10/weatherapi.js
@@ -4,5 +4,11 @@ fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
     console.log(jsObject);
-    document.getElementById('current-temp').textContent = jsObject.main.temp.toFixed(0);
+    document.getElementById("current-temp").textContent = jsObject.main.temp.toFixed(0);
+
+    const imagesrc = "https://openweathermap.org/img/w/" + jsObject.weather[0].icon + ".png";
+    const desc = jsObject.weather[0].description;
+    document.getElementById("imagesrc").textContent = imagesrc;
+    document.getElementById('icon').setAttribute('src', imagesrc);
+    document.getElementById('icon').setAttribute('alt', desc);
   });
