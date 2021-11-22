cd083ce6c08341a712202c206785ff058794a0f7
Ronnyone1991 <rod21007@byui.edu>
2021-11-21 20:36:58 -0400

    js weather

--- lesson-10/js/weatherapi-preston.js
+++ lesson-10/js/weatherapi-preston.js
@@ -10,6 +10,9 @@ fetch(apiURL)
     document.getElementById("windspeed").textContent = 'Wind Speed: ' + jsObject.wind.speed + ' mph';
     document.getElementById("temperature").textContent = "Temp: " + jsObject.main.temp.toFixed(0) + " °F";
                 
-    
-
+    //wind shill calculation  
+    let s = jsObject.wind.speed;
+    let t = jsObject.main.temp;
+    let f = 35.47 + 0.6215 * t - 35.75 * (s ** 0.16) + 0.4275 * (s ** 0.16);
+    document.getElementById("windshill").innerHTML = 'Wind Shill: ' + f.toFixed(0) + " °F";
   });
