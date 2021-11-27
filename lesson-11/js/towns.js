cc2ee6e9a3dcc4bf21d538eaa16ed98caa4f5df4
Ronnyone1991 <rod21007@byui.edu>
2021-11-27 19:27:56 -0400

    code update

--- lesson-11/js/towns.js
+++ lesson-11/js/towns.js
@@ -25,21 +25,21 @@ fetch(townsRequestURL)
 
         let townjson = document.createElement("section");
         let h1 = document.createElement("h1");
-        let h3 = document.createElement("h3");
+        let h2 = document.createElement("h2");
         let img = document.createElement("img");
         let yearf = document.createElement("p");
         let population = document.createElement("p");
         let annualRain = document.createElement("p");
 
         h1.textContent = `${town[count].name}`;
-        h3.textContent = `${town[count].motto} `;
+        h2.textContent = `${town[count].motto} `;
         yearf.textContent = `Year Founded: ${town[count].yearFounded}`;
         population.textContent = `Current Population: ${town[count].currentPopulation}`;
         annualRain.textContent = `Annual Rain Fall: ${town[count].averageRainfall}`;
         img.setAttribute("src", imgTown);
         img.setAttribute("alt", `${town[count].name} `);
         townjson.appendChild(h1);
-        townjson.appendChild(h3);
+        townjson.appendChild(h2);
         townjson.appendChild(yearf);
         townjson.appendChild(population);
         townjson.appendChild(annualRain);
