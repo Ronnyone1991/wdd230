966c14edca0ea5e920cb967a7a8d0cbb90584448
Ronnyone1991 <rod21007@byui.edu>
2021-12-19 21:22:28 -0400

    up

---
+++ project/js/directory.js
@@ -0,0 +1,35 @@
+const requestURL =
+  "https://github.com/Ronnyone1991/wdd230/blob/master/project/json/directory.json";
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject); // temporary checking for valid response and data parsing
+    const president = jsonObject["prophets"];
+    president.forEach((president) => {
+      // create elements
+      let card = document.createElement("section");
+      let h2 = document.createElement("h2");
+      let birthday = document.createElement("p");
+      let birthplace = document.createElement("p");
+      let img = document.createElement("img");
+
+      // add JSON data and display it to the user
+      h2.textContent = `${president.name} ${president.lastname}`;
+      birthday.textContent = `Date of Birth: ${president.birthdate}`;
+      birthplace.textContent = `Place of Birth: ${president.birthplace}`;
+      img.setAttribute("src", president.imageurl);
+      img.setAttribute(
+        "alt",
+        `${president.name} ${president.lastname} - ${president.order}`
+      );
+
+      card.appendChild(h2);
+      card.appendChild(birthday);
+      card.appendChild(birthplace);
+      card.appendChild(img);
+      document.querySelector(".cards").appendChild(card);
+    });
+  });
